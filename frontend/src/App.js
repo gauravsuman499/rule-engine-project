f0533be861a1e842d912c2881536108977ebab3a
gauravsuman499 <116598523+gauravsuman499@users.noreply.github.com>
2024-10-26 11:55:31 +0530

    Add files via upload
    Added App.js file

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+// src/App.js
+import React from 'react';
+import RuleForm from './components/RuleForm';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <RuleForm />
+    </div>
+  );
+};
+
+export default App;
